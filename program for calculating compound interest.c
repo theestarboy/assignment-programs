7b70418a332ae92cdbc3de108cf069b66a9845be
theestarboy <muthonivincent15@gmail.com>
2024-10-05 00:31:19 +0300

    program for calculating compound interest.c

--- program for calculating compound interest.c
+++ program for calculating compound interest.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 #include <math.h> //For power function pow()
 
 int main() {
     float principal,rate,time,compoundInterest,finalAmount;
-    int n; // Number of times the interest is compounded per year
+    
+    int n;
+    // Number of times the interest is compounded per year
     //input values for principal,time,rate,and number of compounding periods
+    
     printf("Enter the principal amount:");
     scanf("%f",&principal);
     printf("Enter the Time in(in years):");
@@ -15,12 +19,17 @@ int main() {
     scanf("%d",&n);
 
     //convert rate from percentage to decimal
+    
     rate=rate/100;
 
     //calculate final amount after compound interest
+    
     finalAmount=principal*pow((1+rate/n),n*time);
+    
     //calculate compound interest
+    
     compoundInterest=finalAmount-principal;
+   
     //display the results
     printf("Final Amount(including interest):%.2f\n",finalAmount);
     printf("compound Interest:%.2f\n",compoundInterest);
