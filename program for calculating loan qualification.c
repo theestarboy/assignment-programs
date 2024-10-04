03e3a0d8771f1ff72c4b555ee7c08cbf4372e919
theestarboy <muthonivincent15@gmail.com>
2024-10-05 00:22:44 +0300

    program for calculating loan qualification.c

--- program for calculating loan qualification.c
+++ program for calculating loan qualification.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
 int main(){
    int age;
    int salary;
+   
    //prompt the user to enter age
+   
    printf("enter the age:");
    scanf("%d",&age);
+   
    //prompt the user to enter the salary
+   
    printf("enter the salary:");
    scanf("%d",&salary);
    if (age>21,salary>21000){
@@ -13,5 +18,6 @@ int main(){
    } else {
        printf("unfortunately,we are unable to offer you aloan at this time");
    }
+   
      return 0;
 }
